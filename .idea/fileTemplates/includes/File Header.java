7df4e4438a9a2e6227c416c01f8179ab9c7fcec1
PanJing <PanJing@Panjing.com>
2019-05-12 21:12:35 +0800

    Initial commit

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,7 @@
+/**
+*
+*   @author Ringleader
+*
+*   @date   ${DATE} - ${TIME}
+*
+*/
---
+++ src/com/atguigu/java/Customer.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author Ringleader
+ * @date 2019/5/12 - 20:42
+ */
+public class Customer {
+}
